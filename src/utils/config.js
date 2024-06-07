c307ad0b2506cdcb79f485d4fe54bf73d905b26f
Sailesh Kalakoti <sailesh.kalakoti@gartner.com>
2024-06-07 15:30:39 +0530

    updated client id

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  client_id: "<YOUR_CLIENT_ID>",
+  client_id: "8d049762e8a0aa08ead6ca7502d4a5b9",
   server_url:
     "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
   account_url: "https://accounts.livechatinc.com/",
